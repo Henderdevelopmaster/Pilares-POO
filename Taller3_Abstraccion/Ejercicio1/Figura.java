1249f083aa441c8ce0a38f444e4b66b32dda2ec9
Henderdevelopermaster <Henderjosegonzalesberal@gmail.com>
2026-08-06 11:12:03 -0500

    Taller3_Abstraccion

---
+++ Taller3_Abstraccion/Ejercicio1/Figura.java
@@ -0,0 +1,13 @@
+public abstract class Figura {
+    protected String nombre;
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+    // Metodo abstracto: las hijas DEBEN implementarlo
+    public abstract double calcularArea();
+    // Metodo concreto: lo heredan todas las hijas
+    public void mostrar() {
+        System.out.println("Figura: " + nombre);
+        System.out.println("Area: " + calcularArea());
+    }
+}
